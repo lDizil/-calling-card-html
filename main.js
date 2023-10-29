0c2d66498a30a412f786bfccef26459a267bc645
Denis <48453680+lDizil@users.noreply.github.com>
2023-10-29 23:55:29 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -4,23 +4,29 @@ const menuList = document.querySelector(".menu__list");
 const menuAbout = document.querySelector("#about");
 const menuPortfolio = document.querySelector("#portfolio");
 
+const Lab2Btn = document.querySelector(".Lab2__btn");
+const Lab2List = document.querySelector(".Lab2__list");
 
 
+const Lab1Btn = document.querySelector(".Lab1__btn");
+const Lab1List = document.querySelector(".Lab1__list");
+
+
+const portfolio_block = document.querySelector('.portfolio__list')
+const portfolioBtn = document.querySelector(".portfolio__btn");
+const portfolioList = document.querySelector(".wrapper");
+
+
+const BackBtn = document.querySelector(".Back__btn");
+const BackList = document.querySelector(".Back__list");
 
 function toggleMenuVisibility() {
     menuList.classList.toggle("hide");
 }
 menuBtn.addEventListener("click", toggleMenuVisibility);
 
-
-const portfolio_block = document.querySelector('.portfolio__list')
-
-
-const portfolioBtn = document.querySelector(".portfolio__btn");
-const portfolioList = document.querySelector(".wrapper");
-
 function toggleportfolioVisibility() {
-    portfolioList.classList.toggle("hide");
+    portfolioList.classList.add("hide");
     portfolio_block.classList.remove("hide")
 
 }
@@ -28,28 +34,20 @@ portfolioBtn.addEventListener("click", toggleportfolioVisibility);
 
 
 
-const Lab1Btn = document.querySelector(".Lab1__btn");
-const Lab1List = document.querySelector(".Lab1__list");
-
-function toggleLab1Visibility() {
-    Lab1List.classList.toggle("hide");
+function toggleLab1Visibility(item) {
+    item.classList.toggle("hide");
 }
-Lab1Btn.addEventListener("click", toggleLab1Visibility);
+Lab1Btn.addEventListener("click", function() {
+    toggleLab1Visibility(Lab1List)
+});
+
+Lab2Btn.addEventListener("click", function() {
+    toggleLab1Visibility(Lab2List)
+});
 
 
-const Lab2Btn = document.querySelector(".Lab2__btn");
-const Lab2List = document.querySelector(".Lab2__list");
-
-function toggleLab2Visibility() {
-    Lab2List.classList.toggle("hide");
-}
-Lab2Btn.addEventListener("click", toggleLab2Visibility);
-
-const BackBtn = document.querySelector(".Back__btn");
-const BackList = document.querySelector(".Back__list");
 
 function toggleBackVisibility() {
-    // Back.classList.toggle("hide");
     portfolio_block.classList.add("hide")
     portfolioList.classList.remove("hide")
 }
